5a29d3b43f4b2ed9bf5b525a6c1932c7318ae478
datadiego <juandiegomariscal@gmail.com>
2024-10-28 09:35:05 +0100

    build

---
+++ create_p5.sh
@@ -0,0 +1,29 @@
+echo "Creando proyecto p5"
+mkdir p5
+echo '
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.10.0/p5.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.10.0/addons/p5.sound.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <meta charset="utf-8" />
+
+  </head>
+  <body>
+    <main>
+    </main>
+    <script src="sketch.js"></script>
+  </body>
+</html>
+' > p5/index.html
+
+echo "
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
+" > p5/sketch.js
---
+++ gitinit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+nombre_proyecto=$1
+
+echo "Valor de entrada: $1"
+
+mkdir $1
+cd $1
+git init
+touch index.html
+touch script.js
+git add .
+git commit -m "build"
+# para ejecutarlo primero damos permisos
+# chmod +x gitinit.sh
